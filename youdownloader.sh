6973bf7c348fccd407234c6af7e7deea15da3a81
Msancho <msanchogin@gmail.com>
2014-12-04 13:45:31 +0100

    Nuevo Script para descargar videos de youtube

---
+++ youdownloader.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+clear
+## Comprobamos si tenemos el software necesario de lo contrario lo instalamos
+if [ ! -x /usr/bin/youtube-dl ]; then
+	echo "No tienes instalado el paquete \"youtube-dl\", vamos a instalarlo"
+	sudo pacman -Sy --noconfirm youtube-dl
+	clear
+fi
+if [ ! -x /usr/bin/ffmpeg ]; then
+	echo "No tienes instalado el paquete \"ffmpeg\", vamos a instalarlo"
+	sudo pacman -Sy --noconfirm ffmpeg
+	clear
+fi
+## Comprobamos si tenemos el directorio de descargas youtube creado, de lo contrario lo crea
+if [ ! -x /home/`echo $USER`/Youtube ];then
+	cd /home/`echo $USER`/ && mkdir Youtube
+fi
+if [ ! -x /home/`echo $USER`/Youtube/Audio ];then
+	cd /home/`echo $USER`/Youtube/ && mkdir Audio
+fi
+if [ ! -x /home/`echo $USER`/Youtube/Videos ];then
+	cd /home/`echo $USER`/Youtube/ && mkdir Videos
+fi
+## Variables de los directorios
+audio="/home/`echo $USER`/Youtube/Audio"
+video="/home/`echo $USER`/Youtube/Videos"
+## Función descargar vídeo
+function video
+{
+	## Escribiremos el link de descarga
+	echo -e "Pegue aquí el link del video que quiere descargar: "
+	read link
+	## Descargamos y obtenemos el nombre del video
+	youtube-dl -o "$video/"'%(title)s.%(ext)s' $link
+	sleep 5 && clear
+}
+## Función extraer audio
+function audio
+{
+	## Escribimos el link
+	echo -e "Pegue aquí el link del audio a extraer: "
+	read link
+	## Extraemos el audio y obtenemos el nombre
+	youtube-dl -o "$audio/"'%(title)s.%(ext)s' --extract-audio --audio-format mp3 --audio-quality 256K $link
+	sleep 5 && clear
+}
+## Menú de opciones
+opt=""
+while [ "$opt" != "3" ]
+do
+	echo 1- Descargar vídeo de Youtube
+	echo 2- Extraer audio de Youtube
+	echo 3- Salir
+	echo
+	read -p "Selecciona una opción: " opt
+	clear
+	if [ "$opt" = "1" ]; then
+		video
+	elif [ "$opt" = "2" ]; then
+		audio
+	elif [ "$opt" = "3" ]; then
+		break
+	else
+		echo "Escoge una opción correcta"
+		sleep 3 && clear
+	fi
+done
+
