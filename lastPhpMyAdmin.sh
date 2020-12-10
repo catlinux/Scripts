098c3e0b7b372f618637e4cfa34d26b78d3d9789
Marc Sancho <marc.catlinux@gmail.com>
2020-12-10 23:16:51 +0100

    Create lastPhpMyAdmin.sh

---
+++ lastPhpMyAdmin.sh
@@ -0,0 +1,5 @@
+
+DATA="$(wget https://www.phpmyadmin.net/home_page/version.txt -q -O-)"
+URL="$(echo $DATA | cut -d ' ' -f 3)"
+VERSION="$(echo $DATA | cut -d ' ' -f 1)"
+wget https://files.phpmyadmin.net/phpMyAdmin/${VERSION}/phpMyAdmin-${VERSION}-all-languages.tar.gz
