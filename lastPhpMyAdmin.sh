e643aa2323461e34f389ca6ee18776b545c3c64e
Marc Sancho <marc.catlinux@gmail.com>
2020-12-10 23:39:51 +0100

    Update lastPhpMyAdmin.sh

--- lastPhpMyAdmin.sh
+++ lastPhpMyAdmin.sh
@@ -4,3 +4,5 @@ DATA="$(wget https://www.phpmyadmin.net/home_page/version.txt -q -O-)"
 URL="$(echo $DATA | cut -d ' ' -f 3)"
 VERSION="$(echo $DATA | cut -d ' ' -f 1)"
 wget https://files.phpmyadmin.net/phpMyAdmin/${VERSION}/phpMyAdmin-${VERSION}-all-languages.tar.gz
+
+tar xvf phpMyAdmin-${VERSION}-all-languages.tar.gz
